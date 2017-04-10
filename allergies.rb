503c601051db20aea8a1c698e52ccd4e4d46a085
ccorrigan <barnumswinter@gmail.com>
2017-04-10 09:18:14 -0600

    allergies exercise initial commit

---
+++ allergies.rb
@@ -0,0 +1,25 @@
+require 'pry'
+score = 3
+       
+allergens = [
+  ["cats", 128], 
+  ["pollen", 64], 
+  ["chocolate", 32], 
+  ["tomatoes", 16], 
+  ["strawberries", 8], 
+  ["shellfish", 4], 
+  ["peanuts", 2], 
+  ["eggs", 1]
+]
+
+allergies = allergens.map do |allergen|
+  if score % allergen[1] < score
+    score -= allergen[1]
+    allergen
+  else
+    nil
+  end
+end       
+
+allergies.compact
+puts allergies        
